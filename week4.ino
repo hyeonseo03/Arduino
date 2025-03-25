e747bd0a83ccbab51b79a8f33becd1700fd701be
hyeonseo03 <gustj10506@gmail.com>
2025-03-25 11:42:20 +0900

    week4,c

---
+++ week4.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Serial.println("I2C Scanner Running...");
+}
+
+void loop() {
+  Serial.println("Scanning...");
+
+  for (byte address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    if (Wire.endTransmission() ==0){
+      Serial.print("I2C 장치 발견: 0x");
+      Serial.println(address, HEX);
+      delay(500);
+    }
+  }
+
+  Serial.println("Scan Complete! Retrying in 5 seconds......\n");
+  delay(5000);
+
+}
